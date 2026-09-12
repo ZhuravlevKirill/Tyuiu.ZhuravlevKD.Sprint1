7a19872c8911d2ad7364cd7a54e53da2737b1a34
ZhuravlevKirill <inciselmao@mail.ru>
2026-09-12 17:58:19 +0500

    Task1

---
+++ Tyuiu.ZhuravlevKD.Sprint1.Task1.V0.Lib/DataService.cs
@@ -0,0 +1,6 @@
+namespace Tyuiu.ZhuravlevKD.Sprint1.Task1.V0.Lib
+{
+    public class DataService
+    {
+    }
+}
---
+++ Tyuiu.ZhuravlevKD.Sprint1.Task1.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace Tyuiu.ZhuravlevKD.Sprint1.Task1.V0.Test
+{
+    [TestFixture]
+    public class DataServiceTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
