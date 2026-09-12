b609cb52ca8c1321a802f663b1250a5f8eb4ef8a
ZhuravlevKirill <inciselmao@mail.ru>
2026-09-12 18:07:44 +0500

    Delete Task0 V5 and Task1 V0

--- Tyuiu.ZhuravlevKD.Sprint1.Task1.V0.Lib/DataService.cs
+++
@@ -1,6 +0,0 @@
-namespace Tyuiu.ZhuravlevKD.Sprint1.Task1.V0.Lib
-{
-    public class DataService
-    {
-    }
-}
--- Tyuiu.ZhuravlevKD.Sprint1.Task1.V0/Program.cs
+++
@@ -1,9 +0,0 @@
-namespace Tyuiu.ZhuravlevKD.Sprint1.Task1.V0
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-        }
-    }
-}
