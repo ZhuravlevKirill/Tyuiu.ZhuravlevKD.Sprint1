9c875a666435c5b5eb9cf1dad51a726064f135d5
ZhuravlevKirill <inciselmao@mail.ru>
2026-09-20 14:40:47 +0500

    Task1

--- Tyuiu.ZhuravlevKD.Sprint1.Task1.V4.Test/DataServiceTest.cs
+++ Tyuiu.ZhuravlevKD.Sprint1.Task1.V4.Test/DataServiceTest.cs
@@ -1,16 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tyuiu.ZhuravlevKD.Sprint1.Task1.V4.Lib;
 
 namespace Tyuiu.ZhuravlevKD.Sprint1.Task1.V4.Test;
 
+[TestClass]
 public class DataServiceTest
 {
-    [Test]
+    [TestMethod]
     public void Calculate()
     {
         DataService ds = new DataService();
 
-        double result = ds.Calculate(2, 4);
+        double x = 2;
+        double y = 4;
 
-        Assert.That(result, Is.EqualTo(12));
+        double result = ds.Calculate(x, y);
+
+        Assert.AreEqual(12, result);
     }
 }
